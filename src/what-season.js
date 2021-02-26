144905d8b6c98dca1c2d0757b59f5b4b01d85681
Elizaveta Panasiuk <lizaveta.panasiuk@gmail.com>
2021-02-26 20:46:12 +0300

    fix: seasons

--- src/what-season.js
+++ src/what-season.js
@@ -1,23 +1,22 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
 module.exports = function getSeason(date) {
-  const month = date.getMonth();
   if(date === undefined){
     return 'Unable to determine the time of year!';
   }
-  if(Object.prototype.toString.call(date) !== '[object Date]'){
+  if (Object.prototype.toString.call(date) !== '[object Date]'){
     throw Error;
   }
-  if(month < 2 || month === 11){
+  if (date.getMonth() < 2 || date.getMonth() === 11) {
     return 'winter';
   }
-  if(month < 5){
-    return 'spring'
+  if (date.getMonth() < 5) {
+    return 'spring';
   }
-  if(month < 8){
+  if (date.getMonth() < 8) {
     return 'summer';
   }
-  if(month < 11){
+  if (date.getMonth() < 11) {
     return 'autumn';
   }
 };
