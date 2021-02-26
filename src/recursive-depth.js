3b7eb7f37582a41b566b7ab874cb5a14e977f63f
Elizaveta Panasiuk <lizaveta.panasiuk@gmail.com>
2021-02-26 16:32:32 +0300

    add: some tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let arrayDepth = 1;
+    for(let elem of arr){
+      let currentArrayDepth = 1;
+      if(Array.isArray(elem)){
+        currentArrayDepth += this.calculateDepth(elem);
+        arrayDepth = currentArrayDepth > arrayDepth ? currentArrayDepth : arrayDepth;
+      }
+    }
+    return arrayDepth;
   }
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  const resultArray = [];
+  let index = 0;
+  while(index < arr.length){
+    let elem = arr[index];
+    if(elem === '--discard-next' && index < arr.length && checkValue(arr[index + 1])){
+      index += 2;
+    }
+    if(elem === '--discard-prev' && index !== 0 && checkValue(arr[index - 1]) && !checkValue(arr[index - 2])){
+      resultArray.pop();
+      index++;
+    }
+    if(elem === '--double-next' && index < arr.length && checkValue(arr[index + 1])){
+      resultArray.push(arr[index + 1]);
+      index++;
+    }
+    if(elem === '--double-prev' && index !== 0 && checkValue(arr[index - 1]) && !checkValue(arr[index - 2])){
+      resultArray.push(arr[index - 1]);
+      index++;
+
+    } 
+    if(checkValue(elem)){
+      resultArray.push(arr[index]);
+      index++;
+    } else{
+      index++;
+    }
+  }
+  function checkValue(value) {
+    return !(value === '--discard-next' || value === '--discard-prev' || value === '--double-next' || value === '--double-prev' || value === undefined);
+  }
+  return resultArray;
 };
