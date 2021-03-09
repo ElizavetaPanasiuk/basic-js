ab2245c578bc8be406908e54157e6aee15aae0fa
Elizaveta Panasiuk <lizaveta.panasiuk@gmail.com>
2021-03-09 11:40:18 +0300

    feat: add solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,57 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  constructor(type = true){
+    this.type = type;
+  };
+
+  encrypt(message, key) {
+    const upMessage = message.toUpperCase();
+    const upKey = key.toUpperCase();
+
+    let result = '';
+
+    if(message === undefined || key === undefined){
+      throw new Error('Arguments error');
+    }
+
+    for (let i = 0, j = 0; i < upMessage.length; i++) {
+      if (/^[A-Z]$/.test(upMessage[i])) {
+        const letterIndex = (upMessage[i].charCodeAt() + (upKey[j % upKey.length]).charCodeAt() - 130) % 26;
+        const letter = this.alphabet.charAt(letterIndex);
+        result = `${result}${letter}`;
+        j++;
+      } else {
+        result = `${result}${upMessage[i]}`;
+      }
+    }
+  
+    return this.type ? result : result.split('').reverse().join('');
+}
+
+
+decrypt(message, key) {
+  const upMessage = message.toUpperCase();
+  const upKey = key.toUpperCase();
+
+  let result = '';
+    if (message === undefined || key === undefined) {
+      throw new Error('Arguments error');
+    }
+
+    for (let i = 0, j = 0; i < upMessage.length; i++) {
+      if (/^[A-Z]$/.test(upMessage[i])) {
+        const letterIndex = (upMessage[i].charCodeAt() - (upKey[j % upKey.length]).charCodeAt() + 104) % 26;
+        const letter = this.alphabet.charAt(letterIndex);
+        result = `${result}${letter}`;
+        j++;
+      } else {
+        result = `${result}${upMessage[i]}`;
+      }
+    }
+    return this.type ? result : result.split('').reverse().join('');
   }
 }
 
