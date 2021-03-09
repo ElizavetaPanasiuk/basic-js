07aa5a271e4baf5441575549a5d7e14f4a28c5dc
Elizaveta Panasiuk <lizaveta.panasiuk@gmail.com>
2021-03-09 12:21:03 +0300

    fix: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,32 +2,21 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
   const resultArray = [];
-  let index = 0;
-  while(index < arr.length){
-    let elem = arr[index];
-    if(elem === '--discard-next' && index < arr.length && checkValue(arr[index + 1])){
-      index += 2;
-    }
-    if(elem === '--discard-prev' && index !== 0 && checkValue(arr[index - 1]) && !checkValue(arr[index - 2])){
-      resultArray.pop();
-      index++;
-    }
-    if(elem === '--double-next' && index < arr.length && checkValue(arr[index + 1])){
-      resultArray.push(arr[index + 1]);
-      index++;
-    }
-    if(elem === '--double-prev' && index !== 0 && checkValue(arr[index - 1]) && !checkValue(arr[index - 2])){
-      resultArray.push(arr[index - 1]);
-      index++;
 
-    } 
-    if(checkValue(elem)){
-      resultArray.push(arr[index]);
-      index++;
-    } else{
-      index++;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== '--double-next' && arr[i] !== '--double-prev' && arr[i] !== '--discard-next' && arr[i] !== '--discard-prev') {
+      resultArray.push(arr[i]);
+    } else if (arr[i] === '--double-next' && checkValue(arr[i + 1])) {
+      resultArray.push(arr[i + 1]);
+    } else if (arr[i] === '--double-prev' && checkValue(arr[i - 1]) && arr[i-2] !== "--discard-next") {
+      resultArray.push(arr[i - 1]);
+    } else if (arr[i] === '--discard-next' && checkValue(arr[i + 1])) {
+      i++;
+    } else if (arr[i] === '--discard-prev' && checkValue(arr[i - 1]) && arr[i-2] !== "--discard-next") {
+      resultArray.pop(arr[i - 1]);
     }
   }
+
   function checkValue(value) {
     return !(value === '--discard-next' || value === '--discard-prev' || value === '--double-next' || value === '--double-prev' || value === undefined);
   }
